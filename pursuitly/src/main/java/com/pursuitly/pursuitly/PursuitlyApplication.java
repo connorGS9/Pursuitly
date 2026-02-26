e505cb60dbb156e3b8368aa7c3408fa12e005ccc
connorGS9 <cs36356@georgiasouthern.edu>
2026-02-26 18:17:01 -0500

    Initial project setup.

---
+++ pursuitly/src/main/java/com/pursuitly/pursuitly/PursuitlyApplication.java
@@ -0,0 +1,13 @@
+package com.pursuitly.pursuitly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PursuitlyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PursuitlyApplication.class, args);
+	}
+
+}
---
+++ pursuitly/src/test/java/com/pursuitly/pursuitly/PursuitlyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pursuitly.pursuitly;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PursuitlyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
